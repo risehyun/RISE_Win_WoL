7a8096f1766f30eac684944bea24b7119dc5f874
DahyunKim <risehyun01@gmail.com>
2023-05-11 23:20:53 +0900

    0511 : InputSystem 기반 수정

---
+++ RISE_Win_WoL/GameEnginePlatform/GameEngineInput.cpp
@@ -0,0 +1,57 @@
+#include "GameEngineInput.h"
+
+GameEngineInput::GameEngineInput()
+{
+}
+
+GameEngineInput::~GameEngineInput()
+{
+}
+
+void GameEngineInput::GameEngineKey::Update(float _DeltaTime)
+{
+	if (true == KeyCheck())
+	{
+		// 키가 눌렸다면
+		PressTime += _DeltaTime;
+
+		// 이전에 키가 눌렸던적이 없다면
+		if (true == Free)
+		{
+			Down = true;
+			Press = true;
+			Up = false;
+			Free = false;
+		}
+
+		else if (true == Down)
+		{
+			Down = false;
+			Press = true;
+			Up = false;
+			Free = false;
+		}
+
+	}
+
+	else
+	{
+		PressTime = 0.0f;
+
+		// 키가 눌리지 않은 경우
+		if (true == Press)
+		{
+			Down = false;
+			Press = false;
+			Up = true;
+			Free = true;
+		}
+		else if (true == Up)
+		{
+			Down = false;
+			Press = false;
+			Up = false;
+			Free = true;
+		}
+	}
+}
---
+++ RISE_Win_WoL/GameEnginePlatform/GameEngineInput.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <Windows.h>
+#include <map>
+#include <string>
+
+class GameEngineInput
+{
+private:
+	class GameEngineKey
+	{
+		bool Down = false;
+		bool Press = false;
+		bool Up = false;
+		bool Free = true;
+
+		float PressTime = 0.0f;
+
+		int Key = -1;
+
+		bool KeyCheck()
+		{
+			return 0 != GetAsyncKeyState(Key);
+		}
+
+		void Update(float _DeltaTime);
+	};
+
+public:
+	GameEngineInput();
+	~GameEngineInput();
+
+	GameEngineInput(const GameEngineInput& _Other) = delete;
+	GameEngineInput(GameEngineInput&& _Other) noexcept = delete;
+	GameEngineInput& operator=(const GameEngineInput& _Other) = delete;
+	GameEngineInput& operator=(GameEngineInput&& _Other) noexcept = delete;
+
+protected:
+
+private:
+	static std::map<std::string, GameEngineKey> AllKeys;
+};
