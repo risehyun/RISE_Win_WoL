5808fe41fb059114d3c9caa9f119b8007f272c68
DahyunKim <risehyun01@gmail.com>
2023-06-22 11:48:22 +0900

    [UPDATE/0622] SnowflakeChakrams 스킬 텍스처 변경, 코드 정리

--- RISE_Win_WoL/RISE_WoL_Contents/SKILL_SnowflakeChakrams.h
+++ RISE_Win_WoL/RISE_WoL_Contents/SKILL_SnowflakeChakrams.h
@@ -1,40 +1,20 @@
 #pragma once
 #include "Skill.h"
+#include <vector>
 
 class SKILL_SnowflakeChakrams : public Skill
 {
-	void SetDir(const float4 & _Dir)
-	{
-		Dir = _Dir;
-	}
-
-	void SetSpeed(const float _Speed)
-	{
-		Speed = _Speed;
-	}
-
-	GameEngineRenderer* Renderer = nullptr;
-
-
-	// 렌더링 테스트용 임시 객체
-	GameEngineRenderer* Renderer2 = nullptr;
-	GameEngineRenderer* Renderer3 = nullptr;
-
-	GameEngineCollision* BodyCollsion = nullptr;
-	GameEngineCollision* BodyCollsion2 = nullptr;
-	GameEngineCollision* BodyCollsion3 = nullptr;
+	std::vector<GameEngineRenderer*> Renderers;
+	std::vector<GameEngineCollision*> Collisions;
 
 private:
 	float4 Dir;
-	float Speed = 500.0f;
+	float4 Range;
 
 	float RollAngle = 45.0f;
-	float Degree;
-	float4 Range;
+	float Radius = 100.0f;
 
 	void Start() override;
 	void Update(float _Delta) override;
-	void Render(float _Delta) override;
-
 };
 
